76099458f5132aa5d4ffdc95e203516016954efe
MFarr <micfar85@gmail.com>
2024-09-03 17:58:55 +0200

    Initial Maven project setup with Spring Boot

---
+++ src/main/java/nl/mfarr/supernova/SupernovaHairsalonApiApplication.java
@@ -0,0 +1,13 @@
+package nl.mfarr.supernova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupernovaHairsalonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupernovaHairsalonApiApplication.class, args);
+    }
+
+}
