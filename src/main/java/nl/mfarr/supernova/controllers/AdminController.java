727510c329716572b424ec1613e0119caef77fd2
Michael Farrah Girgis <micfar85@gmail.com>
2024-09-23 19:43:49 +0200

    added preauthorize to admin  endpoints

--- src/main/java/nl/mfarr/supernova/controllers/AdminController.java
+++ src/main/java/nl/mfarr/supernova/controllers/AdminController.java
@@ -5,6 +5,7 @@ import nl.mfarr.supernova.dtos.AdminResponseDto;
 import nl.mfarr.supernova.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -16,15 +17,17 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<AdminResponseDto> createAdmin(@RequestBody AdminRequestDto adminRequestDto) {
         AdminResponseDto adminResponse = adminService.createAdmin(adminRequestDto);
         return ResponseEntity.ok(adminResponse);
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{email}")
     public ResponseEntity<AdminResponseDto> getAdminByEmail(@PathVariable String email) {
         Optional<AdminResponseDto> adminResponse = adminService.getAdminByEmail(email);
         return adminResponse.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+
 }
