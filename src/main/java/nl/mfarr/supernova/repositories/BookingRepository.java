f76b22be9440e574b5c2f5f63851455cbeb1034e
Michael Farrah Girgis <micfar85@gmail.com>
2024-10-05 19:55:34 +0200

    bookingCustomerRequestDto, and repo set

--- src/main/java/nl/mfarr/supernova/repositories/BookingRepository.java
+++ src/main/java/nl/mfarr/supernova/repositories/BookingRepository.java
@@ -1,1 +1,9 @@
+package nl.mfarr.supernova.repositories;
 
+import nl.mfarr.supernova.entities.BookingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<BookingEntity, Long> {
+}
